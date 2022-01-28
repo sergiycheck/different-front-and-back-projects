9fcd973494de0b30a3d09f9b0938a3d8db3ed12e
serhii.kuzmych <sergiycheck@gmail.com>
2022-01-28 17:32:17 +0200

    add logging optino

--- 5_angular/src/app/state/middleware-meta.reducer.ts
+++ 5_angular/src/app/state/middleware-meta.reducer.ts
@@ -1,9 +1,12 @@
 import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
 
 const debug = (reducer: ActionReducer<any>): ActionReducer<any> => {
   return (state, action) => {
-    console.log('previous state', state);
-    console.log('current action', action);
+    if (environment.logging) {
+      console.log('previous state', state);
+      console.log('current action', action);
+    }
 
     return reducer(state, action);
   };
